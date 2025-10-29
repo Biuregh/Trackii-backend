f63dfb50973128d717495f48b7c56d27593c48fa
Biu-Parse <b.parseghian1991@gmail.com>
2025-10-28 20:04:51 -0700

    name exported async function  for clearer stack traces

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,11 @@
 const mangoose = require("mongoose")
-try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("Mongo connected")
-} catch (e) {
-    console.error("Mongo connect error:", e.message)
-    process.exitCode(1)
+
+module.exports = async function connectDB() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Mongo connected")
+    } catch (e) {
+        console.error("Mongo connect error:", e.message)
+        process.exit(1)
+    }
 }
